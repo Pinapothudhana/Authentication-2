f95dffe12ea197b48de289b93fe01c5b8e5a5104
dhanasekhar <dhanasekhar843@gmail.com>
2024-12-14 05:41:14 +0000

    first commit

---
+++ src/components/Cart/index.js
@@ -0,0 +1,16 @@
+import Header from '../Header'
+import './index.css'
+
+const Cart = () => (
+  <>
+    <Header />
+    <div className="bg-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+        alt="cart"
+        className="image"
+      />
+    </div>
+  </>
+)
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,16 @@
+import Header from "../Header"
+import './index.css'
+
+const Products = () => (
+  <>
+  <Header/>
+  <div className="bg-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+      alt="products"
+      className="image"
+    />
+  </div>
+  </>
+)
+export default Products
